6182b4e981bb4b7b4f2ab508c04aafe74e282a19
Henryawd345 <henrygoodboy2000@gmail.com>
2025-12-07 03:14:46 -0800

    search key completed

--- main.cpp
+++ main.cpp
@@ -59,7 +59,17 @@ void search_key(const map<int, list<string>> &hash_table){
     } else {
         cout << "key FOUND in bucket index " << index << ".\n";
     }
+}
 
+void add_key(map<int, list<string>> &hash_table){
+    string key;
+    cout << "Enter key to add: ";
+    cin >> key;
+
+    int index = gen_hash_index(key);
+    hash_table[index].push_back(key);
+
+    cout << "Key \"" << key << "\" added to bucket index " << index << ".\n";
 }
 
 int main() {
@@ -73,7 +83,48 @@ int main() {
     map<int, list<string>> hash_table;
     string code;
     
-    
+    while (fin >> code){
+        int index = gen_hash_index(code);
+        hash_table[index].push_back(code);
+    }
+    fin.close();
+
+    int choice = 0;
+
+    do{
+        cout << "\n===== HASH TABLE MENU =====\n";
+        cout << "1. Print first 100 entries\n";
+        cout << "2. Search for a key\n";
+        cout << "3. Add a key (coming soon)\n";
+        cout << "4. Remove a key (coming soon)\n";
+        cout << "5. Modify a key (coming soon)\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice){
+            case 1:
+                print_100(hash_table);
+                break;
+            case 2:
+                search_key(hash_table);
+                break;
+            case 3:
+            case 4:
+            case 5:
+                cout << "This option is not implemented yet in this step.\n";
+                break;
+            case 6:
+                cout << "Exiting program...\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } 
+    while (choice != 6);
+
+
+
     return 0;
 } 
 /* 
